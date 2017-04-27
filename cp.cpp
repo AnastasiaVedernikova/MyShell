f15f872f9bc618164ec1f5e427d13882844308e6
OlyaKorabliova <korablyova@ucu.edu.ua>
2017-04-27 13:40:04 +0300

    Update cp.cpp

--- cp.cpp
+++ cp.cpp
@@ -1,81 +1,85 @@
 //
-// Created by anastasia on 19.04.17.
+// Created by Olya Korabliova on 12.04.17.
 //
+
 #include "cp.h"
+
 using namespace std;
-bool cp(string copy_from, string copy_to) {
+
+void cp(string copy_from, string copy_to) {
     string s_path, d_path;
-    vector<string> lst;
-    string s1 = "";
+    string source_file_name, dest_file_name;
+    vector<string> lst;         //vector of file names to copy
+    string s_directory, d_directory;
 
-    std::vector<std::string> source_path;
-    boost::split(source_path, copy_from, boost::is_any_of("/"));
+    size_t num_of_slash_from = count(copy_from.begin(), copy_from.end(), '/');  //calculate amount of / in copy_from
+    size_t num_of_slash_to = count(copy_to.begin(), copy_to.end(), '/');    //calculate amount of / in copy_to
+    size_t num_of_br = count(copy_from.begin(), copy_from.end(), '{') +
+                       count(copy_from.begin(), copy_from.end(), '}');     //calculate amount of {} in copy_from
 
-    std::vector<std::string> destination_path;
-    boost::split(destination_path, copy_to, boost::is_any_of("/"));
 
-    string source_file_name = source_path[source_path.size() - 1];
-
-    for (int i = 1; i < source_path.size() - 1; i++){           // directory to copy from
-        s_path += '/';
-        s_path += source_path[i];
+    if (num_of_slash_from == 0) {       //address (copy_from) entered without directory
+        s_path = curr_dir;
+        source_file_name = copy_from;
     }
 
-    for (int i = 1; i < destination_path.size() - 1; i++){      // directory to copy in
-        d_path += '/';
-        d_path += destination_path[i];
-    }
+    else {
+        vector<string> source_path;
+        boost::split(source_path, copy_from, boost::is_any_of("/"));
+        source_file_name = source_path[source_path.size() - 1];
 
-    if (fs::is_directory(s_path) && fs::is_directory(d_path)) {      // check if the given string contains right directory
-//        cout << "check" << endl;
-        string s = "";
-        size_t num_of_br = count(copy_from.begin(), copy_from.end(), '{') +
-                           count(copy_from.begin(), copy_from.end(), '}');     //calculate amount of {} in copy_from
-
-        if (num_of_br == 2) {       // copy_from contains more than one file to copy
-
-            int i = 0;
-            while (i != source_file_name.length()) {
-                if (source_file_name[i] == '{') {
-                    s = source_file_name.substr(i + 1, source_file_name.length() - 2);      // string of names of files to copy
-                    break;
-                }
-                i++;
-
-            }
-
-            for (int i = 0; i <= s.length(); ++i) {
-
-                if (s[i] != ' ' && i != s.length()) {
-                    s1 += s[i];             // form a string - name of a file to add to lst
-                } else {
-                    lst.push_back(s1);      // vector that contains all names of files to be copied
-                    s1 = "";
-                }
-
-            }
-
-            for (size_t i = 0; i < lst.size(); ++i) {
-                string s_directory = s_path + "/";         // form full directory to the file
-                string d_directory = d_path + "/";         // form full directory to the copied file
-
-                s_directory += lst[i];                     // add name of file to the directory
-                d_directory += lst[i];                     // add name of file to the directory
-
-                fs::path source(s_directory);
-                fs::path destination(d_directory);
-                fs::copy_file(source, destination, fs::copy_option::overwrite_if_exists);
-            }
-
+        for (int i = 1; i < source_path.size() - 1; i++) {       // directory to copy from
+            s_path += '/';
+            s_path += source_path[i];
         }
-        else {
-            fs::path source(copy_from);
-            fs::path destination(copy_to);
+    }
+
+    if (num_of_slash_to == 0) {       //address (copy_to) entered without directory
+        d_path = curr_dir;
+        dest_file_name = copy_to;
+    }
+
+    else {
+        vector<string> destination_path;
+        boost::split(destination_path, copy_to, boost::is_any_of("/"));
+        for (int i = 1; i < destination_path.size() - 1; i++) {      // directory to copy in
+            d_path += '/';
+            d_path += destination_path[i];
+        }
+        dest_file_name = destination_path[destination_path.size() - 1];
+    }
+
+
+    if (num_of_br == 2) {       // copy_from contains more than one file to copy
+        string s1;
+        string s = source_file_name.substr(1, source_file_name.length() - 2);
+        for (int i = 0; i <= s.length(); ++i) {
+            if (s[i] != ' ' && i != s.length()) {
+                s1 += s[i];             // form a string - name of a file to add to lst
+            } else {
+                lst.push_back(s1);      // vector that contains all names of files to be copied
+                s1 = "";
+            }
+        }
+    }
+
+    if (lst.size() != 0) {
+        for (size_t i = 0; i < lst.size(); ++i) {
+            s_directory = s_path + "/" + lst[i];         // form full directory to the file
+            d_directory = d_path + "/" + lst[i];         // form full directory to the copied file
+
+            fs::path source(s_directory);
+            fs::path destination(d_directory);
             fs::copy_file(source, destination, fs::copy_option::overwrite_if_exists);
         }
     }
-    else
-        cout << "No such directory!"<< endl;
+    else {
+        s_directory = s_path + "/" + source_file_name;         // form full directory to the file
+        d_directory = d_path + "/" + dest_file_name;         // form full directory to the copied file
 
+        fs::path source(s_directory);
+        fs::path destination(d_directory);
+        fs::copy_file(source, destination, fs::copy_option::overwrite_if_exists);
+    }
+    cout << "File successfully copied!" << endl;
 }
-
